8f18ca6f2168fd0a7899aa7de89521b7a24283c3
mouataz hakkou <moataz.hakkou@gmail.com>
2021-09-13 15:57:49 +0100

    edut user profile setted

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\Type\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
@@ -30,11 +33,29 @@ class HomeController extends AbstractController
     /**
      * @Route("/editprofile", name="edit_profile")
      */
-    public function editProfile(): Response
+    public function editProfile(Request $request, UserPasswordEncoderInterface $passwordEncoder ): Response
     {
         $user = $this->getUser();
+        $form = $this->createForm(TaskType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData($user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )));
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('profile');
+            
+        }
+
         return $this->render('home/editProfile.html.twig', [
             'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 
